d5fe9fdb9985b0a7b9fe06eb9420f79f575a6104
zaki <zakipr.roni29@gmail.com>
2022-02-24 04:33:05 +0900

    Fimberの導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'dart:async';
+import 'package:fimber/fimber.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'app.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  // Fimber (ロギング用のライブラリ)
+  if (!kReleaseMode) {
+    Fimber.plantTree(DebugTree());
+  } else {
+    debugPrint = (message, {wrapWidth}) {};
+  }
+  // アプリ内で発生したエラーを捕捉
+  runZonedGuarded(
+    () => runApp(const ProviderScope(child: MyApp())),
+    (error, stackTrace) {
+      Fimber.e(error.toString());
+    },
+  );
 }
